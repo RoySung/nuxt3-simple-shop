4c21909bc1ff6d3221b87dd4f82741fca3ca4a9c
RoySung <roysunghan@gmail.com>
2022-10-29 02:15:30 +0000

    Deploying to gh-pages from @ RoySung/nuxt3-simple-shop@b222319e06f22885faa6baee67bdf1b919c6d243 🚀

--- _nuxt/default.1d6ee769.js
+++
@@ -1,1 +0,0 @@
-import{a as t,o,f as r,x as a}from"./entry.bf17df26.js";const n={};function s(e,c){return o(),r("div",null,[a(e.$slots,"default")])}const f=t(n,[["render",s]]);export{f as default};
---
+++ _nuxt/index.43594009.js
@@ -0,0 +1,1 @@
+import{a as e,b as o,e as t,o as a,f as n}from"./entry.7047cc94.js";const s=n("h1",null,"Welcome to the homepage",-1),c=[s],d=e({__name:"index",setup(_){return o("/products"),(r,l)=>(a(),t("div",null,c))}});export{d as default};
